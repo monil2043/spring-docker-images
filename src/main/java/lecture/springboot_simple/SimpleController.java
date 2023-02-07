bc030f09eec40e2d1d75baf4c2931c67de077616
monil2043 <monil.ranka@gmail.com>
2023-02-07 22:08:52 +0530

    new change made

--- src/main/java/lecture/springboot_simple/SimpleController.java
+++ src/main/java/lecture/springboot_simple/SimpleController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
+import org.json.simple.JSONObject;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SimpleController {
@@ -17,11 +19,47 @@ public class SimpleController {
 
     @GetMapping("/greet/{name}")
     public ResponseEntity<String> greetUser(@PathVariable String name) {
-        String res = "Hello 7Feb with docker, " + name;
+        String res = "Hello 7thFeb try21, " + name;
         return ResponseEntity.ok(res);
     }
+	
+	@GetMapping("/covid")
+    public JSONObject getHelloClient1() {
+        final String uri = "https://api.covid19api.com/";
 
-    @GetMapping("/aston")
+        RestTemplate restTemplate = new RestTemplate();
+        JSONObject result = restTemplate.getForObject(uri, JSONObject.class);
+
+        System.out.println(result);
+        
+        return result;
+    }
+	
+	@GetMapping("/employees")
+    public JSONObject getHelloClient2() {
+        final String uri = "https://dummy.restapiexample.com/api/v1/employees";
+
+        RestTemplate restTemplate = new RestTemplate();
+        JSONObject result = restTemplate.getForObject(uri, JSONObject.class);
+
+        System.out.println(result);
+        
+        return result;
+    }
+	
+	@GetMapping("/employees/{id}")
+    public JSONObject getHelloClient(@PathVariable Integer id) {
+        final String uri = "https://dummy.restapiexample.com/api/v1/employee/" + id ;
+
+        RestTemplate restTemplate = new RestTemplate();
+        JSONObject result = restTemplate.getForObject(uri, JSONObject.class);
+
+        System.out.println(result);
+        
+        return result;
+    }
+
+    @GetMapping("/ascii")
     public ResponseEntity<String> randomAscii() {
         return ResponseEntity.ok(generateRandomAscii(10000));
     }
